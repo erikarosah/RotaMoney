98e570ac25b0cd5415a6612b26d0b5eb425d3e24
erikarosah <erikarosamg@gmail.com>
2023-12-08 09:06:45 -0300

    initial styling

---
+++ src/styles/global.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #202024;
+        --shape: #29292E;
+        --header: #121214;
+
+        --white: #FFFFFF;
+        --text-title: #E1E1E6;
+        --text-placeholder: #7c7c8a;
+
+        --green: #00875F;
+        --green-dark: #015F43;
+        --green-light: #00B37E;
+
+        --red: #F75A69;
+        --red-dark: #AA2834;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media (max-width: 1000px) {
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%;
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
